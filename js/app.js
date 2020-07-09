4512d33ee2db6c4d7b39de45bb2a2403f3425aca
S1EpoY <slepoy1992@gmail.com>
2020-07-09 03:44:51 +0300

    unloading a completed test task

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const browserWidth = window.innerWidth
+const img = document.querySelector(".js-arcticle");
+
+if(browserWidth >= 1440) {
+    img.src="../images/Broshure_dsctp.png"
+}
+
+window.addEventListener("resize", getSize)
+
+function getSize() {
+    const width = document.documentElement.clientWidth;
+
+    if(width >= 1440) {
+        img.src="../images/Broshure_dsctp.png"
+    } else {
+        img.src="../images/Broshure.png"
+    }
+
+    return
+}
+
+
